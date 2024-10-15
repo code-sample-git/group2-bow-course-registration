cdc25c10cefcb5a2eb4647cce0256e223a85ad77
Michael Ng <8717608+code-sample-git@users.noreply.github.com>
2024-10-15 09:55:00 -0600

    remove inappropriate language from Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to Bow Course Registration. Fuck you</h1>
+      <h1>Welcome to Bow Course Registration.</h1>
       <p>Select a program or course to get started.</p>
     </div>
   );
